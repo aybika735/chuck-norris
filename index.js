18173877fc492111ae2df8df31794cca83dc161a
aybika <adamovaaybika@mail.ru>
2022-09-22 22:47:30 +0300

    chuck

--- index.js
+++ index.js
@@ -3,25 +3,18 @@
         try{ const list = document.querySelector("#list");
         const button = document.querySelector('button');
         button.addEventListener('click', async function(){
-
-
-
         const res = await fetch('https://api.chucknorris.io/jokes/random ', {
-            method: "GET",
-            headers:{
-                "Content-type": "application/json; charset=UTF-8",
+        method: "GET",
+         headers:{
+         "Content-type": "application/json; charset=UTF-8",
             }
         });
         const data = await res.json();
-
-         
-            const div = document.createElement('div');
-            div.textContent = data.value;
-            list.append(div); 
+        const div = document.createElement('div');
+        div.textContent = data.value;
+        list.append(div); 
            
         })
-
-        console.log(data.value);
 
     }catch(error){
         console.log(error);
@@ -29,7 +22,4 @@
 }
 
 Chuck();
-function refreshPage(){
-    window.location.reload();
-} 
 
